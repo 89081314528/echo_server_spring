ec68fb79f218611c38b154b90ea5510d77a1b9f7
julia <89081314528@mail.ru>
2021-11-18 11:55:23 +0300

    add run

---
+++ src/test/java/com/example/echo_server_spring/EchoServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echo_server_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
